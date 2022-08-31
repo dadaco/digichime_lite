9f43350018dc949866f5a011e3b860f106c5e62e
だだこ <111478498+dadaco@users.noreply.github.com>
2022-09-01 06:29:58 +0900

    Add files via upload

--- js/jQ.js
+++ js/jQ.js
@@ -4,6 +4,7 @@ $(function () {
   })
 });
 
+
 function FixedAnime() {
   var scroll = $(window).scrollTop();
   if (scroll >= 1){
@@ -16,6 +17,16 @@ function FixedAnime() {
 $('header a').click(function () {
   var elmHash = $(this).attr('href');
   var pos = Math.round($(elmHash).offset().top-60);
+  $('body,html').animate({scrollTop: pos}, 500);
+	if($('header .wrap nav').hasClass('open')){
+		$('header .wrap nav, .btn-line').removeClass('open');
+	}
+  return false;
+});
+
+$('.movie a').click(function () {
+  var elmHash = $(this).attr('href');
+  var pos = Math.round($(elmHash).offset().top-70);
   $('body,html').animate({scrollTop: pos}, 500);
   return false;
 });
@@ -34,5 +45,18 @@ $(function() {
 	$('#faq dl dt').on("click", function() {
 		$(this).next('dd').slideToggle();
 		$(this).parent('dl').toggleClass('active');
+	});
+});
+
+$(function(){
+	$(window).scroll(function (){
+		$('.fadein').each(function(){
+			var elemPos = $(this).offset().top;
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll > elemPos - windowHeight + 200){
+				$(this).addClass('scrollin');
+			}
+		});
 	});
 });
--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 // JavaScript Document
 
-
-// タブレットでPCを表示する用
+// タブレットでPC表示
 
 var ua = navigator.userAgent;
 if ((ua.indexOf('iPhone') > 0) || ua.indexOf('iPod') > 0 || (ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0)) {
   document.write('<meta name="viewport" content="width=device-width">');
 } else {
-  document.write('<meta name="viewport" content="width=1180">');
+  document.write('<meta name="viewport" content="width=1286">');
 }
 
